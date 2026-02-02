703fb37a005f946b396d085a8f3c9498104295fc
Thorsten Kukuk <kukuk@suse.com>
2026-02-02 15:32:30 +0100

    add_extra_partition.sh: automatically find next partition number

--- images/scripts/add_extra_partition.sh
+++ images/scripts/add_extra_partition.sh
@@ -6,8 +6,8 @@ IMG_NAME=${1}
 PART_SIZE=${2:-"8G"}
 LABEL=${3:-"images"}
 FS_TYPE=${4:-"ext4"}
-PART_EXTRA_SPACE="2M"
 SGDISK="/usr/sbin/sgdisk"
+NEW_PART_NR=2
 
 usage() {
     echo "Usage: $0 <image_file> [[[<size>] <label>] <fs_type>]"
@@ -15,6 +15,21 @@ usage() {
     echo "  <label>   is the filesystem label, defaults to $LABEL"
     echo "  <fs_type> must be 'xfs' or 'ext4'"
     echo "Example: $0 disk.img 16G ext4"
+}
+
+get_next_part_nr()
+{
+    local last_partnr
+
+    last_partnr=$("$SGDISK" -p "$IMG_NAME" 2>/dev/null | awk '/^[[:space:]]*[0-9]/ {print $1}' | sort -rn | head -1)
+    if [ $? -ne 0 ] || [ -z "$last_partnr" ]; then
+	echo "Error: Failed to read partition table from '$IMG_NAME'."
+	exit 1
+    fi
+
+    ((last_partnr++))
+
+    NEW_PART_NR="$last_partnr"
 }
 
 if [ "$1" == "--help" ] || [ "$1" == "-h" ]; then
@@ -26,6 +41,7 @@ if [ -z "$IMG_NAME" ]; then
     echo "Error: no image provided."
     usage
     exit 1
+fi
 
 if [ ! -f "$IMG_NAME" ]; then
     echo "Error: File '$IMG_NAME' not found."
@@ -39,10 +55,12 @@ if [[ "$FS_TYPE" != "xfs" && "$FS_TYPE" != "ext4" ]]; then
     exit 1
 fi
 
+get_next_part_nr
+
 echo "Extending image file..."
 truncate -s "+$PART_SIZE" "$IMG_NAME"
 # and some more for GPT overhead
-truncate -s "+$PART_EXTRA_SPACE" "$IMG_NAME"
+#truncate -s "+$PART_EXTRA_SPACE" "$IMG_NAME"
 
 # Move the GPT backup header to the end of the file
 echo "Relocating GPT backup header..."
@@ -51,10 +69,10 @@ $SGDISK -e "$IMG_NAME" > /dev/null
 echo "Creating new $PART_SIZE partition..."
 # -n 2:0:+8G -> New partition #2, start at first available sector (0), size +PART_SIZE
 # -t 2:8300  -> Set type of partition 2 to Linux Filesystem (8300)
-$SGDISK -n 2:0:+"$PART_SIZE" -c 2:"data" -t 2:8300 "$IMG_NAME" > /dev/null
+$SGDISK -n "${NEW_PART_NR}":0:+"$PART_SIZE" -c "${NEW_PART_NR}":"data" -t "${NEW_PART_NR}":8300 "$IMG_NAME" > /dev/null
 
 # Get the starting sector of partition 2
-START_SECTOR=$($SGDISK -i 2 "$IMG_NAME" | grep "First sector:" | awk '{print $3}')
+START_SECTOR=$($SGDISK -i "$NEW_PART_NR" "$IMG_NAME" | grep "First sector:" | awk '{print $3}')
 # Get the logical sector size (usually 512, but best to check)
 SECTOR_SIZE=$($SGDISK -p "$IMG_NAME" | grep "Sector size" | awk '{print $4}' | grep -o '[0-9]*')
 # If detection fails, default to 512
