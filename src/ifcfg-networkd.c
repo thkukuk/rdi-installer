87cacd2913f86a4f7c1bd6191effadba05ee4b22
Thorsten Kukuk <kukuk@suse.com>
2026-02-04 10:09:04 +0100

    ifcfg-networkd: fix wrong program name

--- src/ifcfg-networkd.c
+++ src/ifcfg-networkd.c
@@ -450,7 +450,7 @@ main(int argc, char *argv[])
           print_help();
           return 0;
         case 'v':
-          printf("expiry (%s) %s\n", PACKAGE, VERSION);
+          printf("ifcfg-networkd (%s) %s\n", PACKAGE, VERSION);
           return 0;
         default:
           print_error();
