ad04fbb15c4c33b7e109b3f20e30bd9d529d5359
Thorsten Kukuk <kukuk@suse.com>
2026-02-10 17:37:54 +0100

    tests: fix tst-ifcfg-networkd*

--- tests/tst-ifcfg-networkd-1.sh
+++ tests/tst-ifcfg-networkd-1.sh
@@ -18,12 +18,12 @@ trap cleanup EXIT
 
 TEMPDIR=$(mktemp -d)
 
-./ifcfg-networkd -o "$TEMPDIR" "ifcfg=*=dhcp" "ifcfg=00:11:22:33:44:55=dhcp,rfc2132" ifcfg='"eth1=192.168.0.2/24 192.158.10.12/24,192.168.0.1,8.8.8.8,mydomain.com"'
+./rdii-networkd -o "$TEMPDIR" "ifcfg=*=dhcp" "ifcfg=00:11:22:33:44:55=dhcp,rfc2132" ifcfg='"eth1=192.168.0.2/24 192.158.10.12/24,192.168.0.1,8.8.8.8,mydomain.com"'
 
 for cfg in "${TEMPDIR}"/*; do
     cfg=$(basename "$cfg")
-    if cmp "$TEMPDIR/$cfg" "tst-ifcfg-networkd-1/$cfg" ; then
-       diff -u "tst-ifcfg-networkd-1/$cfg" "$TEMPDIR/$cfg"
+    if ! cmp "$TEMPDIR/$cfg" "../tests/tst-ifcfg-networkd-1/$cfg" ; then
+       diff -u "../tests/tst-ifcfg-networkd-1/$cfg" "$TEMPDIR/$cfg"
        exit 1
     fi
 done
--- tests/tst-ifcfg-networkd-2.sh
+++ tests/tst-ifcfg-networkd-2.sh
@@ -20,12 +20,12 @@ trap cleanup EXIT
 
 TEMPDIR=$(mktemp -d)
 
-./ifcfg-networkd -o "$TEMPDIR" "ifcfg=eth0.66=10.0.1.1/24,10.0.1.254" "ifcfg=eth0.67=dhcp" "ifcfg=eth1.33=dhcp"
+./rdii-networkd -o "$TEMPDIR" "ifcfg=eth0.66=10.0.1.1/24,10.0.1.254" "ifcfg=eth0.67=dhcp" "ifcfg=eth1.33=dhcp"
 
 for cfg in "${TEMPDIR}"/*; do
     cfg=$(basename "$cfg")
-    if cmp "$TEMPDIR/$cfg" "tst-ifcfg-networkd-2/$cfg" ; then
-       diff -u "tst-ifcfg-networkd-2/$cfg" "$TEMPDIR/$cfg"
+    if ! cmp "$TEMPDIR/$cfg" "../tests/tst-ifcfg-networkd-2/$cfg" ; then
+       diff -u "../tests/tst-ifcfg-networkd-2/$cfg" "$TEMPDIR/$cfg"
        exit 1
     fi
 done
